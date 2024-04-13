cf99b3c4f74cc556b604de498208192daa5c67cf
joyanedel <joyanede@alumnos.inf.utfsm.cl>
2024-04-13 10:39:16 -0400

    Parse http head

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
-use std::io::{Read, Write};
+use std::{
+    io::{Read, Write},
+    net::TcpStream,
+};
 
 mod requests;
 mod responses;
@@ -6,14 +9,23 @@ mod server;
 
 fn main() {
     let tcp_server = server::create_server();
-    let mut req_buffer = [0; 512];
 
     for stream in tcp_server.incoming() {
         let mut stream = stream.unwrap();
-        stream.read(&mut req_buffer);
-        println!("Request: {}", String::from_utf8_lossy(&req_buffer));
+        handle_request(&mut stream);
         let response = responses::format_response("THX".to_string(), 200).unwrap();
         stream.write(response.as_bytes()).unwrap();
         stream.flush().unwrap();
     }
+}
+
+fn handle_request(req_stream: &mut TcpStream) {
+    let mut req_buffer = [0; 512];
+    req_stream.read(&mut req_buffer).unwrap();
+    let parsed_stream = String::from_utf8_lossy(&req_buffer);
+
+    let http_lines: Vec<&str> = parsed_stream.split("\n").collect();
+    let result = requests::get_http_head(&http_lines);
+
+    println!("R: {:?}", result);
 }
--- src/requests.rs
+++ src/requests.rs
@@ -1,4 +1,7 @@
+use regex::Regex;
+
 const HTTP_VERSION: &'static str = "1.1";
+const HTTP_HEAD: &'static str = "(GET|POST|PUT|PATCH|DELETE|OPTIONS|HEAD) ([\\S])+ HTTP.*";
 
 pub fn format_request(method: String, path: String) -> Result<String, ()> {
     let method = verify_method(&method)?;
@@ -16,4 +19,17 @@ fn verify_method(method: &str) -> Result<&str, ()> {
         "OPTIONS" => Ok("OPTIONS"),
         _ => Err(()),
     }
+}
+
+pub fn get_http_head<'a>(lines: &Vec<&'a str>) -> Option<&'a str> {
+    let http_head_regex = Regex::new(HTTP_HEAD).unwrap();
+    for &line in lines.iter() {
+        if !http_head_regex.is_match(line.trim()) {
+            continue;
+        };
+
+        return Some(&line);
+    }
+
+    None
 }
